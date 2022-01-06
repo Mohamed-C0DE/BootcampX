06f0c8f16536bf9d079f704f8a99b416acefa710
Mohamed Ali <12maali27@gmail.com>
2022-01-06 22:05:57 +0000

    parameterized students.js queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,25 +7,23 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-const args = process.argv;
-console.log(args);
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool
-  .query(
-    `
-SELECT students.id, students.name AS student_name, cohorts.name AS cohort_name
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '${args[2]}%'
-LIMIT ${args[3]};
-`
-  )
-  .then((res) => {
-    console.log(res);
-    res.rows.forEach((user) => {
-      //   console.log(user);
-      console.log(
-        `${user.student_name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`
-      );
-    });
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((user) => {
+    console.log(
+      `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+    );
   });
+});
