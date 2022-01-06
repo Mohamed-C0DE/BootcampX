a10f18d7d3d0b31a79024277b35278153e69af58
Mohamed Ali <12maali27@gmail.com>
2022-01-06 22:06:14 +0000

    parameterized teachers.js queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,24 +7,22 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-const args = process.argv;
-console.log(args);
+const queryString = `
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = $1
+GROUP BY teacher, cohort
+ORDER BY teacher;
+`;
 
-pool
-  .query(
-    `
-    SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
-    FROM teachers
-    JOIN assistance_requests ON teachers.id = teacher_id
-    JOIN students ON students.id = student_id
-    JOIN cohorts ON cohorts.id = cohort_id
-    WHERE cohorts.name = '${args[2]}'
-    GROUP BY teacher, cohort
-    ORDER BY teacher;
-`
-  )
-  .then((res) => {
-    res.rows.forEach((user) => {
-      console.log(`${user.cohort}: ${user.teacher}`);
-    });
+const cohortName = process.argv[2];
+const values = [cohortName];
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((user) => {
+    console.log(`${user.cohort}: ${user.teacher}`);
   });
+});
